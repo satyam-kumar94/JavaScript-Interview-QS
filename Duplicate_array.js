eaa02891835f280b4728192ed46e1151799eba40
satyam <tech.kumarsatyam@gmail.com>
2023-08-31 14:19:26 +0530

    commit

---
+++ Duplicate_array.js
@@ -0,0 +1,4 @@
+function duplicate(arr){
+    return arr.concat(arr);
+}
+console.log(duplicate([1,2,3,4,5]));
---
+++ reverse_number.js
@@ -0,0 +1,4 @@
+function rev(num){
+      return  Number ( num.toString().split("").reverse().join(""))
+}
+console.log(rev(1522));
